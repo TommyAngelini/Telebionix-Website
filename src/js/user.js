71440a89f4330b278029b73246dc68871b7a3cb4
Tomas Angelini <tangelin@usc.edu>
2022-03-02 18:23:45 -0800

    tested circular animations, worked a bit on features page buttons

--- src/js/user.js
+++ src/js/user.js
@@ -2,3 +2,14 @@
 // user.js
 // Use this to write your custom JS
 //
+
+$(function() {
+
+    $(".btn-features").on("click", function() {
+      //hide all sections
+      $(".content-section").hide();
+      //show the section depending on which button was clicked
+      $("#" + $(this).attr("data-section")).show();
+    });
+  
+  });
