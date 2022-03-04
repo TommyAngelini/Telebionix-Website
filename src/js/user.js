68142f203617edacab2f2d837b6cf9a98a5b3a26
Tomas Angelini <tangelin@usc.edu>
2022-03-03 19:17:38 -0800

    fixed remosense features section, transitions working now

--- src/js/user.js
+++ src/js/user.js
@@ -6,12 +6,20 @@
 $(function() {
 
     $(".btn-features").on("click", function() {
+
       //hide all sections
-      $(".content-section").hide();
+      $(".content-section").hide()
       //show the section depending on which button was clicked
-      $("." + $(this).attr("data-section")).show();
+      $("." + $(this).attr("data-section")).fadeIn()
 
       console.log(this);
     });
   
   });
+
+
+window.onload=function(){
+    document.getElementById("linkid").click();
+    $(".section2").fadeOut();
+    
+  };
