7a74413f4a22556324b853c2e1a7d57dee174d21
Tomas Angelini <tangelin@usc.edu>
2022-03-05 14:53:29 -0800

    fixed buttons, navbar, footer, added UX features

--- src/js/user.js
+++ src/js/user.js
@@ -12,6 +12,10 @@ $(function() {
       //show the section depending on which button was clicked
       $("." + $(this).attr("data-section")).fadeIn()
 
+      $('.btn-features').not(this).removeClass('activeb');    
+      $(this).toggleClass('activeb');
+      e.preventDefault();
+
       console.log(this);
     });
   
@@ -23,3 +27,22 @@ window.onload=function(){
     $(".section2").hide();
     
   };
+
+  $('.Button').click(function(e) {
+    $('.Button').not(this).removeClass('active');    
+    $(this).toggleClass('active');
+    e.preventDefault();
+});
+
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("hamburgericon").style.top = "0";
+  } else {
+    document.getElementById("hamburgericon").style.top = "-50px";
+  }
+}
