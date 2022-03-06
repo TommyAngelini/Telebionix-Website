e3dbf76bfcd2ba476b9c39850cab803bbdc5f441
Tomas Angelini <tangelin@usc.edu>
2022-03-05 18:38:03 -0800

    changed investor video

--- src/js/user.js
+++ src/js/user.js
@@ -22,6 +22,7 @@ $(function() {
   });
 
 
+
 window.onload=function(){
     document.getElementById("linkid").click();
     $(".section2").hide();
@@ -35,14 +36,3 @@ window.onload=function(){
 });
 
 
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("hamburgericon").style.top = "0";
-  } else {
-    document.getElementById("hamburgericon").style.top = "-50px";
-  }
-}
